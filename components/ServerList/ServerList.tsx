7f62fafab020367634abadbbf1608feae2bdd561
Cleophus Alves <cleophus775@gmail.com>
2025-01-14 09:26:36 +0530

    server pics

--- components/ServerList/ServerList.tsx
+++ components/ServerList/ServerList.tsx
@@ -8,26 +8,28 @@ export default function ServerList(): JSX.Element {
       {
         id: uuid(),
         name: 'Test Server 1',
-        image: ''
+        image: 'https://images.unsplash.com/photo-1735263759278-ed793ef6f1e7?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
       {
         id: uuid(),
         name: 'Test Server 2',
-        image: ''
+        image: 'https://images.unsplash.com/photo-1735327854928-6111ac6105c8?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
       {
         id: uuid(),
         name: 'Test Server 3',
-        image: ''
+        image: 'https://images.unsplash.com/photo-1735192395661-9358e7d8cb75?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
     ];
-  return <div className="bg-green-400">
+  return <div className='bg-dark-gray h-full flex flex-col items-center'>
     {servers.map((server) => (
       <button 
-      key={server.id} 
+      key={server.id}
+      className='p-4'
       onClick={() => console.log(server.name)}>
         {server.image && checkIfUrl(server.image) ? (
           <Image
+          className='rounded-icon'
           src={server.image}
           width={50}
           height={50}
