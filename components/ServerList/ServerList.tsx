e8a219a247c8a28c2351c724fa496964f83e8a09
Cleophus Alves <cleophus775@gmail.com>
2025-01-14 10:33:28 +0530

    selected icon

--- components/ServerList/ServerList.tsx
+++ components/ServerList/ServerList.tsx
@@ -1,22 +1,23 @@
 import { EnmityServer } from '@/models/EnmityServer';
-import { JSX } from 'react';
+import { JSX, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import Image from 'next/image';
 
 export default function ServerList(): JSX.Element {
+  const [activeServer, setActiveServer] = useState<EnmityServer| undefined>();
     const servers: EnmityServer[] = [
       {
-        id: uuid(),
+        id: '1',
         name: 'Test Server 1',
         image: 'https://images.unsplash.com/photo-1735263759278-ed793ef6f1e7?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
       {
-        id: uuid(),
+        id: '2',
         name: 'Test Server 2',
         image: 'https://images.unsplash.com/photo-1735327854928-6111ac6105c8?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
       {
-        id: uuid(),
+        id: '3',
         name: 'Test Server 3',
         image: 'https://images.unsplash.com/photo-1735192395661-9358e7d8cb75?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       },
@@ -25,8 +26,11 @@ export default function ServerList(): JSX.Element {
     {servers.map((server) => (
       <button 
       key={server.id}
-      className='p-4'
-      onClick={() => console.log(server.name)}>
+      className={`p-4 sidebar-icon ${
+        server.id === activeServer?.id ? 'selected-icon' : ''
+      }`}
+      onClick={() => setActiveServer(server)}
+      >
         {server.image && checkIfUrl(server.image) ? (
           <Image
           className='rounded-icon'
