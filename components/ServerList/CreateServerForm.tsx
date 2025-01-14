720dad943c060b57b2d436f15395f201acb9a714
Cleophus Alves <cleophus775@gmail.com>
2025-01-14 23:14:15 +0530

    write server name

--- components/ServerList/CreateServerForm.tsx
+++ components/ServerList/CreateServerForm.tsx
@@ -1,12 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { JSX } from 'react';
 import Link from 'next/link';
 import CloseIcon from '../Icons';
 import { useSearchParams } from 'next/navigation';
+import { UserObject } from '@/models/UserObject';
+
+type FormState = {
+    serverName: string;
+    serverImage: string;
+    users: UserObject[];
+};
+
 export default function CreateServerForm(): JSX.Element {
     const params = useSearchParams();
     const showCreateServerForm = params.get('createServer');
     const dialogRef = useRef<HTMLDialogElement>(null);
+
+    const initialState: FormState = {
+        serverName: '',
+        serverImage: '',
+        users: [],
+    };
+    const [formData, setFormData] = useState<FormState>(initialState);
 
     useEffect(() => {
         if (showCreateServerForm && dialogRef.current) {
@@ -26,6 +41,24 @@ export default function CreateServerForm(): JSX.Element {
                 <CloseIcon />
                 </Link>
             </div>
+            <form method='dialog' className='flex flex-col space-y-2 px-6'>
+                <label className='labelTitle' htmlFor='serverName'>
+                    Server Name
+                </label>
+                <div className='flex items-center bg-gray-100'>
+                    <span className='text-2xl p-2 text-gray-500'>#</span>
+                    <input 
+                    type='text'
+                    id='serverName'
+                    name='serverName'
+                    value={formData.serverName}
+                    onChange={(e) =>
+                        setFormData({ ...formData, serverName: e.target.value })
+                    }
+                    required
+                    />
+                </div>
+            </form>
         </dialog>
     );
 }
