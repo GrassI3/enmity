6749828eb3f88f23d3ed2c551d825a738ff0f89a
Cleophus Alves <cleophus775@gmail.com>
2025-01-17 22:46:15 +0530

    no dinner

--- components/ServerList/ServerList.tsx
+++ components/ServerList/ServerList.tsx
@@ -1,45 +1,65 @@
 import { EnmityServer } from '@/models/EnmityServer';
-import { JSX, useState } from 'react';
+import { JSX, useCallback, useState, useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 import Image from 'next/image';
 import Link from 'next/link';
 import CreateServerForm from './CreateServerForm';
+import { useChatContext } from 'stream-chat-react';
+import { channel } from 'diagnostics_channel';
+import { Channel } from 'stream-chat';
 
 export default function ServerList(): JSX.Element {
-  const [activeServer, setActiveServer] = useState<EnmityServer| undefined>();
-    const servers: EnmityServer[] = [
-      {
-        id: '1',
-        name: 'Test Server 1',
-        image: 'https://images.unsplash.com/photo-1735263759278-ed793ef6f1e7?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      },
-      {
-        id: '2',
-        name: 'Test Server 2',
-        image: 'https://images.unsplash.com/photo-1735327854928-6111ac6105c8?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      },
-      {
-        id: '3',
-        name: 'Test Server 3',
-        image: 'https://images.unsplash.com/photo-1735192395661-9358e7d8cb75?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      },
-    ];
+  const { client } = useChatContext();
+  const [activeServer, setActiveServer] = useState<EnmityServer | undefined>();
+  const [serverList, setServerList] = useState<EnmityServer[]>([]);
+  
+  const loadServerList = useCallback(async (): Promise<void> => {
+    const channels = await client.queryChannels({
+      type: 'messaging',
+      members: { $in: [client.userID as string] },
+    });
+    const serverSet: Set<EnmityServer> = new Set(
+      channels
+        .map((channel: Channel) => {
+          return {
+            id: channel.data?.data?.id,
+            name: (channel.data?.data?.server as string) ?? 'Unknown',
+            image: channel.data?.data?.image,
+          };
+        })
+        .filter((server: EnmityServer) => server.name != 'Unknown')
+        .filter(
+          (server: EnmityServer, index, self) =>
+            index ===
+            self.findIndex((serverObject) => serverObject.name == server.name)
+        )
+    );
+    const serverArray = Array.from(serverSet.values());
+    setServerList(serverArray);
+    if (serverArray.length > 0) {
+      setActiveServer(serverArray[0]);
+    }
+  }, [client]);
+
+  useEffect(() => {
+    loadServerList();
+  }, [loadServerList]);
+
   return <div className='bg-dark-gray h-full flex flex-col items-center'>
-    {servers.map((server) => (
-      <button 
-      key={server.id}
-      className={`p-4 sidebar-icon ${
-        server.id === activeServer?.id ? 'selected-icon' : ''
-      }`}
-      onClick={() => setActiveServer(server)}
+    {serverList.map((server) => (
+      <button
+        key={server.id}
+        className={`p-4 sidebar-icon ${server.id === activeServer?.id ? 'selected-icon' : ''
+          }`}
+        onClick={() => setActiveServer(server)}
       >
         {server.image && checkIfUrl(server.image) ? (
           <Image
-          className='rounded-icon'
-          src={server.image}
-          width={50}
-          height={50}
-          alt='Server Icon'
+            className='rounded-icon'
+            src={server.image}
+            width={50}
+            height={50}
+            alt='Server Icon'
           />
         ) : (
           <span className='rounded-icon bg-gray-600 w-[50px] flex items-center justify-center text-sm text-white'>
@@ -49,8 +69,8 @@ export default function ServerList(): JSX.Element {
       </button>
     ))}
     <Link
-    href={'/?createServer=true'}
-    className='flex items-center justify-center rounded-icon bg-white text-green-500 hover:bg-green-500 hover:text-white hover:rounded-xl transition-all duration-200 p-2 my-2 text-2xl font-light h-12 w-12'
+      href={'/?createServer=true'}
+      className='flex items-center justify-center rounded-icon bg-white text-green-500 hover:bg-green-500 hover:text-white hover:rounded-xl transition-all duration-200 p-2 my-2 text-2xl font-light h-12 w-12'
     >
       <span className='inline-block'>+</span>
     </Link>
